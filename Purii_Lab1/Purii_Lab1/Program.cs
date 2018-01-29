53e9545b93b021ead1117283e1df338e27910ab1
Ruslan Purii <ruslanpurii@gmail.com>
2018-01-29 22:31:08 +0200

    Added Lab 1

---
+++ Purii_Lab1/Purii_Lab1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purii_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create list of students
+            List<Student> studList = new List<Student>();
+            studList.Add(new Student("Karpenko"));
+            studList.Add(new Student("Kamarov"));
+            studList.Add(new Student("Lisko"));
+            studList.Add(new Student("Deruk"));
+            studList.Add(new Student("Kostenko"));
+
+            // fill students' grades with random int 61-100
+            Random rnd = new Random();
+            string[] subjects = new string[] { "OOP", "FP", "LP", "ODM" };
+
+            foreach(Student stud in studList)
+            {
+                foreach(string subject in subjects)
+                {
+                    stud.AddGrade(subject, rnd.Next(61, 100));
+                }
+            }
+
+            // sort in descending order
+            studList.Sort((a, b) => b.GetAverageGrage().CompareTo(a.GetAverageGrage()));
+
+            // log results in console
+            foreach(Student stud in studList)
+            {
+                Console.WriteLine(stud);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Purii_Lab1/Purii_Lab1/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Purii_Lab1
+{
+    class Student
+    {
+        private string surname;
+        private List<Grade> grades;
+
+        public Student(string surname)
+        {
+            this.surname = surname;
+            grades = new List<Grade>();
+        }
+
+        public void AddGrade(string subject, int grade)
+        {
+            grades.Add(new Grade(subject, grade));
+        }
+
+        public double GetAverageGrage()
+        {
+            return (double) grades.ConvertAll<int>((a) => a.grade).Sum() / grades.Count();
+        }
+
+        public override string ToString()
+        {
+            string stud = surname + ": " + GetAverageGrage();
+            foreach(Grade grade in grades)
+            {
+                stud += "\n[" + grade.subject + ": " + grade.grade + "]";
+            }
+            stud += "\n\n";
+            return stud;
+        }
+
+        public class Grade
+        {
+            public string subject;
+            public int grade;
+
+            public Grade(string subject, int grade)
+            {
+                this.subject = subject;
+                this.grade = grade;
+            }
+        }
+
+    }
+}
